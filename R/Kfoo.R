fd298a6917a6331fae0a693e865275ebadf4f345
jagm03 <jmonsalv@uji.es>
2017-11-01 20:55:47 +0100

    First modification basic function

---
+++ R/Kfoo.R
@@ -0,0 +1,30 @@
+#	Kfoo.R		Estimation of K function as data frame with numerator 
+#           and denominator separately
+#
+#	$Revision: 1 $	$Date: 2017/11/01 20:52:01 $
+#
+# -------- standard arguments ------------------------------	
+#	pattern		point pattern (of class 'ppp')
+#
+#	r		      distance values at which to compute K	
+#
+# -------- standard output ------------------------------
+#      A data frame with columns named
+#
+#	num:		numerator of estimated K function
+#
+#	den:		denominator of estimated K function
+#
+#	val:		K function estimated by the given correction
+
+# ------------------------------------------------------------------------
+
+Kfoo <- function (pattern, r, correction = "isotropic",...){
+  verifyclass(pattern, "ppp")
+  K <- Kest(pattern, r = r, correction = correction, ratio = T, ...)
+  num <- attr(K, which = "numerator")
+  num <- num[, names(num)!= "theo"]
+  den <- attr(K, which = "denominator")
+  den <- den[, names(den) != "theo"]
+  return(K = data.frame(num = num$iso, den = den$iso, val = K$iso))
+}
